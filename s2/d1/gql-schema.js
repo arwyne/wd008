34b4a23e28d81ddc88e07f8f7d85e7f022879593
Arwyne De Guzman <arwynedeguzman@gmail.com>
2020-11-05 16:49:39 +0800

    updated notes

--- s2/d1/gql-schema.js
+++ s2/d1/gql-schema.js
@@ -1,18 +1,16 @@
-const graphql = require('graphql');
-const { 
-    GraphQLObjectType,
-    GraphQLID,
-    GraphQLString,
-    GraphQLList,
-    GraphQLSchema,
-    GraphQLBoolean,
-    GraphQLNonNull
- } = graphql;
+const graphql = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLList,
+  GraphQLSchema,
+  GraphQLBoolean,
+  GraphQLNonNull,
+} = graphql;
 
- const graphqlIsoDate = require('graphql-iso-date');
- const {
-     GraphQLDateTime
- } = graphqlIsoDate
+const graphqlIsoDate = require("graphql-iso-date");
+const { GraphQLDateTime } = graphqlIsoDate;
 
 /*
     Declare graphql as dependencies
@@ -21,9 +19,9 @@ const {
         - Basically, we can extract data from arrays and objects & assign them into variables
 */
 
-const Member = require('./models/member')
-const Team = require('./models/team')
-const Task = require('./models/task')
+const Member = require("./models/member");
+const Team = require("./models/team");
+const Task = require("./models/task");
 
 /*
     Mongoose models
@@ -35,20 +33,20 @@ const Task = require('./models/task')
 */
 
 const TeamType = new GraphQLObjectType({
-    name: 'Team',
-    fields: () => ({
-        id: {type: GraphQLID},
-        name: {type: GraphQLString },
-        createdAt: {type: GraphQLDateTime},
-        updatedAt: {type: GraphQLDateTime},
-        tasks: {
-            type: new GraphQLList(TaskType),
-            resolve: (parent, args) => {
-                return Task.find({teamID: parent.id})
-            }
-        }
-    })
-})
+  name: "Team",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    createdAt: { type: GraphQLDateTime },
+    updatedAt: { type: GraphQLDateTime },
+    tasks: {
+      type: new GraphQLList(TaskType),
+      resolve: (parent, args) => {
+        return Task.find({ teamID: parent.id });
+      },
+    },
+  }),
+});
 
 /*
     Activity: 05/28/2020
@@ -60,35 +58,34 @@ const TeamType = new GraphQLObjectType({
 */
 
 const MemberType = new GraphQLObjectType({
-    name: 'Member',
-    fields: () => ({
-        id: {type: GraphQLID},
-        firstName: {type: GraphQLString},
-        lastName: {type: GraphQLString},
-        position: {type: GraphQLString},
-        createdAt: {type: GraphQLDateTime},
-        updatedAt: {type: GraphQLDateTime}
-    })
-})
-
+  name: "Member",
+  fields: () => ({
+    id: { type: GraphQLID },
+    firstName: { type: GraphQLString },
+    lastName: { type: GraphQLString },
+    position: { type: GraphQLString },
+    createdAt: { type: GraphQLDateTime },
+    updatedAt: { type: GraphQLDateTime },
+  }),
+});
 
 const TaskType = new GraphQLObjectType({
-    name: 'Task',
-    fields: () => ({
-        id: {type: GraphQLID},
-        description: {type: GraphQLString},
-        teamID: {type: GraphQLString},
-        isCompleted: {type: GraphQLBoolean},
-        createdAt: {type: GraphQLDateTime},
-        updatedAt: {type: GraphQLDateTime},
-        team: {
-            type: TeamType,
-            resolve: (parent, args) => {
-                return Team.findById(parent.teamID)
-            }
-        }
-    })
-})
+  name: "Task",
+  fields: () => ({
+    id: { type: GraphQLID },
+    description: { type: GraphQLString },
+    teamID: { type: GraphQLString },
+    isCompleted: { type: GraphQLBoolean },
+    createdAt: { type: GraphQLDateTime },
+    updatedAt: { type: GraphQLDateTime },
+    team: {
+      type: TeamType,
+      resolve: (parent, args) => {
+        return Team.findById(parent.teamID);
+      },
+    },
+  }),
+});
 
 /*
     GraphQLObjectType = will define our model/structure records from our MongoDB server
@@ -103,54 +100,64 @@ const TaskType = new GraphQLObjectType({
 */
 
 const RootQuery = new GraphQLObjectType({
-    name: 'Query',
-    fields: {
-        // To retrieve all the documents from Team Collection
-        teams: {
-            type: new GraphQLList(TeamType),
-            /* resolve - contains the logic retrieving our documents. It has two resolvers, parents and args
+  name: "Query",
+  fields: {
+    // To retrieve all the documents from Team Collection
+    teams: {
+      type: new GraphQLList(TeamType),
+      /* resolve - contains the logic retrieving our documents. It has two resolvers, parents and args
                     resolver 'parent', will get the data from its parent Object Type
                     resolver 'args', will receive an arguments or data from the (). This is for creating mutations or querying specific record.
                         ex. team (arguments: value) {
                             properties/fields of the specific record
                         }
             */
-            resolve: (parent, args) => {
-                return Team.find({})
-            }
-        },
-        team: {
-            type: TeamType,
-            args: {
-                id: {type: GraphQLID}
-            },
-            resolve: (parent, args) => {
-                return Team.findById(args.id)
-            }
-        },
-        members: {
-            type: new GraphQLList(MemberType),
-            resolve: (parent, args) => {
-                return Member.find({})
-            }
-        },
-        tasks: {
-            type: new GraphQLList(TaskType),
-            resolve: (parent, args) => {
-                return Task.find({})
-            }
-        },
-        task: {
-            type: TaskType,
-            args: {
-                id: {type: GraphQLID}
-            },
-            resolve: (parent, args) => {
-                return Task.findById(args.id)
-            }
-        }
-    }
-})
+      resolve: (parent, args) => {
+        return Team.find({});
+      },
+    },
+    team: {
+      type: TeamType,
+      args: {
+        id: { type: GraphQLID },
+      },
+      resolve: (parent, args) => {
+        return Team.findById(args.id);
+      },
+    },
+    members: {
+      type: new GraphQLList(MemberType),
+      resolve: (parent, args) => {
+        return Member.find({});
+      },
+    },
+    member: {
+      type: MemberType,
+      args: {
+        id: { type: GraphQLID },
+      },
+      resolve: (parents, args) => {
+        return Member.findById(args.id);
+      },
+    },
+
+    tasks: {
+      type: new GraphQLList(TaskType),
+      resolve: (parent, args) => {
+        return Task.find({});
+      },
+    },
+    task: {
+      type: TaskType,
+      args: {
+        id: { type: GraphQLID },
+      },
+      resolve: (parent, args) => {
+        return Task.findById(args.id);
+      },
+    },
+  },
+});
 
 /*
     GraphQLList - a wrapping type which points to another type. Will create a lists base on the defined fields of an Object Type
@@ -159,54 +166,54 @@ const RootQuery = new GraphQLObjectType({
 /* 
     Mutation -  for add, update, and delete records
 */
-const Mutation = new GraphQLObjectType ({
-    name: 'Mutation',
-    fields: {
-        addTeam: {
-            type: TeamType,
-            args: {
-                /* GraphQLNonNull will check if the value of name property is null */
-                name: {type: new GraphQLNonNull(GraphQLString)},
-            },
-            resolve: (parent, args) => {
-                /* instantiate a new Team that will handle the value (args.name) for name property */
-                let newTeam = new Team({
-                    name: args.name,
-                })
-            /* Save the instantiated new Team by using its variable 'newTeam' and save method */
-            return newTeam.save()
-            }
-        },
-        updateTeam: {
-            type: TeamType,
-            args: {
-                id: {type: new GraphQLNonNull(GraphQLID)},
-                name: {type: new GraphQLNonNull(GraphQLString)},
-            },
-            resolve: (parents, args) => {
-                let teamID = {_id: args.id}
-                let updateName = {name: args.name}
-                // db.teams.update({_id: "id of specific team"}, {$set{name:"new Value"}})
-                // findOneAndUpdate(condition, update, callback function)
-                // function(team){} -> call back function, receives the result from the update which is the parameter of function(team)
-                return Team.findOneAndUpdate(teamID, updateName, function(team){
-                    return team
-                })
-            }
-        },
-        deleteTeam: {
-            type: TeamType,
-            args: {
-                id: {type: new GraphQLNonNull(GraphQLID)}
-            },
-            resolve: (parent, args)=> {
-                let teamID = { _id: args.id}
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addTeam: {
+      type: TeamType,
+      args: {
+        /* GraphQLNonNull will check if the value of name property is null */
+        name: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve: (parent, args) => {
+        /* instantiate a new Team that will handle the value (args.name) for name property */
+        let newTeam = new Team({
+          name: args.name,
+        });
+        /* Save the instantiated new Team by using its variable 'newTeam' and save method */
+        return newTeam.save();
+      },
+    },
+    updateTeam: {
+      type: TeamType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLID) },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve: (parents, args) => {
+        let teamID = { _id: args.id };
+        let updateName = { name: args.name };
+        // db.teams.update({_id: "id of specific team"}, {$set{name:"new Value"}})
+        // findOneAndUpdate(condition, update, callback function)
+        // function(team){} -> call back function, receives the result from the update which is the parameter of function(team)
+        return Team.findOneAndUpdate(teamID, updateName, function (team) {
+          return team;
+        });
+      },
+    },
+    deleteTeam: {
+      type: TeamType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLID) },
+      },
+      resolve: (parent, args) => {
+        let teamID = { _id: args.id };
 
-                return Team.findOneAndDelete(teamID)
-            }
-        },
+        return Team.findOneAndDelete(teamID);
+      },
+    },
 
-        /*
+    /*
             Activity (May 29)
             Instructions:
 
@@ -234,100 +241,120 @@ const Mutation = new GraphQLObjectType ({
                 - args: id, firstName, lastName, position
         */
 
-        addMember: {
-            type: MemberType,
-            args: {
-                firstName: {type: new GraphQLNonNull(GraphQLString)},
-                lastName: {type: new GraphQLNonNull(GraphQLString)},
-                position: {type: new GraphQLNonNull(GraphQLString)}
-            },
-            resolve: (parent, args) => {
-                let newMember = new Member({
-                    firstName: args.firstName,
-                    lastName: args.lastName,
-                    position: args.position
-                })
-            return newMember.save()
-            }
-        },
+    addMember: {
+      type: MemberType,
+      args: {
+        firstName: { type: new GraphQLNonNull(GraphQLString) },
+        lastName: { type: new GraphQLNonNull(GraphQLString) },
+        position: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve: (parent, args) => {
+        let newMember = new Member({
+          firstName: args.firstName,
+          lastName: args.lastName,
+          position: args.position,
+        });
+        return newMember.save();
+      },
+    },
 
-        // updateMember: {
-        //     type: MemberType,
-        //     args: {
-        //         id: {type: new GraphQLNonNull(GraphQLID)},
-        //         firstName: {type: new GraphQLNonNull(GraphQLString)},
-        //         lastName: {type: new GraphQLNonNull(GraphQLString)},
-        //         position: {type: new GraphQLNonNull(GraphQLString)}
-        //     },
-        //     resolve: (parents, args) => {
-        //         let teamID = {_id: args.id}
-        //         let firstName = {firstName: args.firstName}
-        //         let lastName = {lastName: args.lastName}
-        //         let position = {position: args.position}
+    deleteMember: {
+      type: MemberType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLID) },
+      },
+      resolve: (parent, args) => {
+        let MemberId = { _id: args.id };
+        return Member.findByIdAndRemove(MemberId);
+      },
+    },
 
-        //         return Member.findOneAndUpdate(teamID, firstName, function(member){
-        //             return member
-        //         })
-        //     }
-        // },
+    updateMember: {
+      type: MemberType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLID) },
+        firstName: { type: new GraphQLNonNull(GraphQLString) },
+        lastName: { type: new GraphQLNonNull(GraphQLString) },
+        position: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve: (parents, args) => {
+        let memberId = { _id: args.id };
+        let member = {
+          firstName: args.firstName,
+          lastName: args.lastName,
+          position: args.position,
+        };
+        return Member.findOneAndUpdate(memberId, member);
 
-        addTask: {
-            type: TaskType,
-            args: {
-                description: {type: new GraphQLNonNull(GraphQLString)},
-                teamID: {type: new GraphQLNonNull(GraphQLID)},
-            },
-            resolve: (parent, args) => {
-                let newTask = new Task({
-                    description: args.description,
-                    teamID: args.teamID
-                })
-            return newTask.save()
-            }
-        },
+        // solution ko dati na mali
+        // let teamID = {_id: args.id}
+        // let firstName = {firstName: args.firstName}
+        // let lastName = {lastName: args.lastName}
+        // let position = {position: args.position}
 
-        updateTask: {
-            type: TaskType,
-            args: {
-                id: {type: new GraphQLNonNull(GraphQLID)},
-                isCompleted: {type: new GraphQLNonNull(GraphQLBoolean)}
-            },
-            resolve: (parents, args) => {
-                let taskID = {_id: args.id}
-                let updateTask = {isCompleted: args.isCompleted}
-                return Task.findOneAndUpdate(taskID, updateTask, function(task){
-                    return task
-                })
-            }
-        },
+        // return Member.findOneAndUpdate(memberId, firstName, function (member) {
+        //   return member;
+        // });
+      },
+    },
 
-        // solution ni mam
-        // updateTask: {
-        //     type: TaskType,
-        //     args: {
-        //         id: {type: new GraphQLNonNull(GraphQLID)}
-        //     },
-        //     resolve: (parents, args) => {
-        //         let taskID = {_id: args.id}
-        //         let updateTask = {isCompleted: true}
-        //         return Task.findOneAndUpdate(taskID, updateTask)
-        //     }
-        // },
+    addTask: {
+      type: TaskType,
+      args: {
+        description: { type: new GraphQLNonNull(GraphQLString) },
+        teamID: { type: new GraphQLNonNull(GraphQLID) },
+      },
+      resolve: (parent, args) => {
+        let newTask = new Task({
+          description: args.description,
+          teamID: args.teamID,
+        });
+        return newTask.save();
+      },
+    },
 
-        deleteTask: {
-            type: TaskType,
-            args: {
-                id: {type: new GraphQLNonNull(GraphQLID)}
-            },
-            resolve: (parent, args)=> {
-                let taskID = { _id: args.id}
-                return Task.findOneAndDelete(taskID)
-            }
-        }
-    }
-})
+    updateTask: {
+      type: TaskType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLID) },
+        isCompleted: { type: new GraphQLNonNull(GraphQLBoolean) },
+      },
+      resolve: (parents, args) => {
+        let taskID = { _id: args.id };
+        let updateTask = { isCompleted: args.isCompleted };
+        return Task.findOneAndUpdate(taskID, updateTask, function (task) {
+          return task;
+        });
+      },
+    },
+
+    // solution ni mam
+    // updateTask: {
+    //     type: TaskType,
+    //     args: {
+    //         id: {type: new GraphQLNonNull(GraphQLID)}
+    //     },
+    //     resolve: (parents, args) => {
+    //         let taskID = {_id: args.id}
+    //         let updateTask = {isCompleted: true}
+    //         return Task.findOneAndUpdate(taskID, updateTask)
+    //     }
+    // },
+
+    deleteTask: {
+      type: TaskType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLID) },
+      },
+      resolve: (parent, args) => {
+        let taskID = { _id: args.id };
+        return Task.findOneAndDelete(taskID);
+      },
+    },
+  },
+});
 
 module.exports = new GraphQLSchema({
-    query: RootQuery,
-    mutation: Mutation
-})
+  query: RootQuery,
+  mutation: Mutation,
+});
--- s2/d1/models/team.js
+++ s2/d1/models/team.js
@@ -1,11 +1,14 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const teamSchema = new Schema({
-    name: {type: String, required:true},
-    taskID: {type: String, required:true}
-}, {
-    timestamps: true
-})
+const teamSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    taskID: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('Team', teamSchema)
+module.exports = mongoose.model("Team", teamSchema);
